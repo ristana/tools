77338785233095fe9c805d9148116280788caeb1
ristana <michaelpaulortega@gmail.com>
2016-01-06 04:31:16 +0100

    fixed move_characters to include contained_by=0

--- db_utility/sql/move_characters.sql
+++ db_utility/sql/move_characters.sql
@@ -1,2 +1,2 @@
-update swg.objects set scene_id='&scene_id',x='&x',y='&y',z='&z' where object_id = (select character_object from swg.players where uc_character_name = '&uc_character_name');
+update swg.objects set scene_id='&scene_id',x='&x',y='&y',z='&z',contained_by=0 where object_id = (select character_object from swg.players where uc_character_name = '&uc_character_name');
 commit;
